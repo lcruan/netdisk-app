b548b79de81c6f32648a882f3da2a52e30d9711c
lcruan <1416802331@qq.com>
2022-12-22 21:43:55 +0800

    初始化项目

---
+++ components/common/nav-bar.vue
@@ -0,0 +1,38 @@
+<template>
+<view>
+  <view class="fixed-top bg-white">
+    <uni-status-bar></uni-status-bar>
+    <view style="height: 44px;" class="flex border-bottom align-center">
+      <view class="flex-1 flex">
+        <slot name="left"></slot>
+      </view>
+      <view class="flex-1 flex justify-center">
+        <slot></slot>
+      </view>
+      <view class="flex-1 flex justify-end">
+        <slot name="right"></slot>
+      </view>
+    </view>
+  </view>
+  <!-- 占位 用于解决导航栏遮挡部分内容-->
+  <view>
+    <!-- 状态栏 -->
+    <uni-status-bar></uni-status-bar>
+    <view style="height: 44px;">
+      
+    </view>
+  </view>
+</view>
+</template>
+
+<script>
+  import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue'
+  export default {
+    components: {
+      uniStatusBar
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+    <!-- 自定义导航栏部分 -->
+    <nav-bar>
+      <text slot="left" class="font-md ml-3">首页</text>
+      <!-- 因为是两个，所以给外侧加一个template -->
+      <template slot="right">
+        <view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
+          <text class="iconfont icon-zengjia"></text>
+        </view>
+        <view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
+          <text class="iconfont icon-gengduo"></text>
+        </view>
+      </template>
+    </nav-bar>
+    
+    <view class="bg-danger" style="height: 1000px;">
+      123
+    </view>
+	</view>
+</template>
+
+<script>
+  
+  import navBar from '@/components/common/nav-bar.vue'
+	export default {
+    components: {
+      navBar
+    },
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
